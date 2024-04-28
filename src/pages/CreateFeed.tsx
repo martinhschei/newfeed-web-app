77e0fc5b92b4c7741f14272520af65f7e79bd6ef
Martin Schei <martinhschei@gmail.com>
2024-04-28 17:58:51 +0200

    WIP

--- src/pages/CreateFeed.tsx
+++ src/pages/CreateFeed.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Sheet from 'react-modal-sheet';
-import { FeedService } from "../services/FeedService.ts";
 import { useNavigate } from "react-router-dom";
+import { FeedService } from "../services/FeedService.ts";
 
 const CreateFeed = () => {
     const navigate = useNavigate();
@@ -9,47 +9,51 @@ const CreateFeed = () => {
     const [feedName, setFeedName] = useState('');
     
     const onSaveNew = async (event: any) => {
-        try {
-          const newFeed: any = await (await FeedService.createFeed(feedName)).json();
-          console.log(newFeed)
-          setOpen(false);
-          navigate('/' + newFeed.data.slug);
-        } catch (error) {
-          console.error("Could not create feed: ", error);
-        }
-      }
-    
-      const onChangeFeedName = (event: any) => {
-        console.log(event.target.value);
-        setFeedName(event.target.value);
-      }
-    
-      const onCreateNew = (event: any) => {
-        setOpen(true);
-      }
-    
-      const onCancelCreate = (event: any) => {
+      try {
+        const newFeed: any = await (await FeedService.createFeed(feedName)).json();
+        console.log(newFeed)
         setOpen(false);
+        navigate('/' + newFeed.data.slug);
+      } catch (error) {
+        console.error("Could not create feed: ", error);
       }
+    }
+    
+    const onChangeFeedName = (event: any) => {
+      console.log(event.target.value);
+      setFeedName(event.target.value);
+    }
+  
+    const onCreateNew = (event: any) => {
+      setOpen(true);
+    }
+  
+    const onCancelCreate = (event: any) => {
+      setOpen(false);
+    }
 
     return (
       <main className="landing">
         <button className="button is-success is-medium" onClick={onCreateNew}>New feed</button>
-        <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
-          <Sheet.Container className="sheet">
-            <Sheet.Header />
-            <Sheet.Content>
-              <div className="create-feed">
-                <input className="input is-medium is-rounded" type="text" value={feedName} name="feedName" onChange={onChangeFeedName} placeholder="Feed name" />
-                <div className="create-feed__options">
-                  <button className="button is-success is-medium" onClick={onSaveNew}>Ok</button>
-                  <button className="button is-medium is-outlined" onClick={onCancelCreate}>Cancel</button>
+        <div className="sheet-wrapper">
+          <Sheet isOpen={isOpen} onClose={() => setOpen(false)} snapPoints={[-50, 0.5, 100, 0]} initialSnap={1} >
+            <Sheet.Container className="sheet">
+              <Sheet.Header />
+              <Sheet.Content>
+                <div className="create-feed-wrapper" style={{display: 'flex', 'justifyContent' : 'center'}}>
+                  <div className="create-feed" style={{minWidth: '350px', maxWidth: '650px'}}>
+                    <input className="input is-medium is-rounded" type="text" value={feedName} name="feedName" onChange={onChangeFeedName} placeholder="Feed name" />
+                    <div className="create-feed__options">
+                      <button className="button is-success is-medium" onClick={onSaveNew}>Start</button>
+                      <button className="button is-medium" onClick={onCancelCreate}>Cancel</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </Sheet.Content>
-          </Sheet.Container>
-          <Sheet.Backdrop />
-        </Sheet>
+              </Sheet.Content>
+            </Sheet.Container>
+            <Sheet.Backdrop />
+          </Sheet>
+        </div>
       </main>
     )
 }
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,11 +1,13 @@
-import { useParams } from "react-router-dom"
-import { IFeed } from "../interface/IFeed.ts"
-import React, { useEffect, useState } from "react"
-import { FeedService } from "../services/FeedService.ts"
+import React, { useEffect, useState } from "react";
+import Sheet from 'react-modal-sheet';
+import { useParams } from "react-router-dom";
+import { IFeed } from "../interface/IFeed.ts";
+import { FeedService } from "../services/FeedService.ts";
 
 const Feed = () => {
     const params = useParams()
     const feedSlug = params.slug
+    const [isOpen, setOpen] = useState(false);
     const [feed, setFeed] = useState({} as IFeed)
     
     useEffect(() => {
@@ -29,7 +31,7 @@ const Feed = () => {
 
     return (
         <div style={styles.main}>
-            <div className="feed" style={styles.feed}>
+            <div style={styles.feed}>
                 <div style={styles.feedHeader}>{feed.name}</div>
                 {feed.posts && feed.posts.map((post, index) => (
                     <div key={index} style={styles.feedPost}>
@@ -38,37 +40,62 @@ const Feed = () => {
                     </div>
                 ))}
                 <div style={styles.feedActions}>
-                    <button className="button is-success is-medium">New post</button>
+                    <button className="button is-success">New post</button>
                 </div>
             </div>
+            <Sheet isOpen={isOpen} onClose={() => setOpen(false)} snapPoints={[-50, 0.5, 100, 0]} initialSnap={1} >
+            <Sheet.Container className="sheet">
+              <Sheet.Header />
+              <Sheet.Content>
+                <div className="create-feed-wrapper" style={{display: 'flex', 'justifyContent' : 'center'}}>
+                  <div className="create-feed" style={{minWidth: '350px', maxWidth: '650px'}}>
+                    <input className="input is-medium is-rounded" type="text" value={feedName} name="feedName" onChange={onChangeFeedName} placeholder="Feed name" />
+                    <div className="create-feed__options">
+                      <button className="button is-success is-medium" onClick={onSaveNew}>Start</button>
+                      <button className="button is-medium" onClick={onCancelCreate}>Cancel</button>
+                    </div>
+                  </div>
+                </div>
+              </Sheet.Content>
+            </Sheet.Container>
+            <Sheet.Backdrop />
+          </Sheet>
         </div>
     )
 }
 
 const styles = {
-    feedHeader: {
-        color: '#333',
-        display: 'flex',    
-        fontSize: '24px',
+    main: {
+        width: '100vh',
+        padding: '10px',
+        display: 'flex',
         justifyContent: 'center',
     },
     feed: {
-        width: '100%',
-        height: '100vh',
+        width: '700px',
+        height: '89vh',
         marginTop: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    feedHeader: {
+        color: '#333',
+        fontSize: '24px',
+        height: '50px',
+    },
+    feedActions: {
+        bottom: '10px',
+        height: '50px',
+        marginTop: '10px',
+        textAlign: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        justifyContent: 'center',
     },
     feedPost: {
         backgroundColor: '#f9f9f9',
         boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
     },
-    feedActions: {
-        positions: 'fixed',
-    },
-    main: {
-        width: '100vh',
-        padding: '10px',
-        display: 'flex',
-    }
 }
 
 export default Feed
